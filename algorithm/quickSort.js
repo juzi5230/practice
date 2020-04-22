a9a3c0ff6a7fc3bdc0cdb8d9a8c9f6620bd979a5
xjcheng4 <xjcheng4@iflytek.com>
2020-04-22 13:38:23 +0800

    快速排序

--- algorithm/quickSort.js
+++ algorithm/quickSort.js
@@ -52,38 +52,65 @@
 // } 
 // Sort([10,10,1,2,4,6,7,89,7,4])
 
-function quickSort(arr, start, end) {
-    var i = start
-    var j = end
-    var temp = arr[start]
-    var mid = 0
-    // console.log(start, end, '....................................')
-    if(start >= end) {
-        return
-    }
-    while(i < j) {
-        if(temp >= arr[j]) {
-            arr[i] = arr[j]
-            i++
-            mid = j
-        }
-        if(temp < arr[i]) {
-            arr[j] = arr[i]
-            j--
-            mid = i
-        }
-        if(temp < arr[j]) {
-            j--
-        }
-    }
-    arr[mid] = temp
-    if(mid > 0) {
-      quickSort(arr, start, mid-1)
-    }
-    quickSort(arr, mid + 1, end)
-}
-let s = [5,8, 9, 3, 2, 6, 90, 12]
-quickSort(s, 0, s.length-1)
-console.log(s)
+// function quickSort(arr, start, end) {
+//     var i = start
+//     var j = end
+//     var temp = arr[start]
+//     var mid = 0
+//     // console.log(start, end, '....................................')
+//     if(start >= end) {
+//         return
+//     }
+//     while(i < j) {
+//         if(temp >= arr[j]) {
+//             arr[i] = arr[j]
+//             i++
+//             mid = j
+//         }
+//         if(temp < arr[i]) {
+//             arr[j] = arr[i]
+//             j--
+//             mid = i
+//         }
+//         if(temp < arr[j]) {
+//             j--
+//         }
+//     }
+//     arr[mid] = temp
+//     if(mid > 0) {
+//       quickSort(arr, start, mid-1)
+//     }
+//     quickSort(arr, mid + 1, end)
+// }
+// let s = [5,8, 9, 3, 2, 6, 90, 12]
+// quickSort(s, 0, s.length-1)
+// console.log(s)
 
-//// 有问题。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+// //// 有问题。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
+
+function quickSort(arr, low, high) {
+    if(low < high) {
+        let ind = getIndex(arr, low, high)
+        quickSort(arr, 0, ind -1, )
+        quickSort(arr, ind + 1, high )
+    }
+}
+function getIndex(arr, low, high) {
+  let temp = arr[low]
+  while(low < high) {
+    while(low < high && arr[high] >= temp) {
+      high--
+    }
+    arr[low] = arr[high]
+    while(low < high && arr[low] <= temp) {
+        low++
+    }
+    arr[high] = arr[low] 
+  }
+  arr[low] = temp
+  return low
+}
+
+var arr = [ 49, 38, 65, 97, 23, 22, 76, 1, 5, 8, 2, 0, -1, 22 ]
+quickSort(arr, 0, arr.length - 1)
+console.log(arr)
