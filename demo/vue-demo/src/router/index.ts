863eaf5c617921b539b543f01e78bedb696da0d0
xjcheng4 <xjcheng4@iflytek.com>
2021-04-26 10:18:22 +0800

    add: .

--- demo/vue-demo/src/router/index.ts
+++ demo/vue-demo/src/router/index.ts
@@ -7,13 +7,15 @@ Vue.use(VueRouter)
 const Animater = () => import('@/components/Animater.vue')
 const ImageShow = () => import('@/components/ImageShow.vue')
 const KeyIndex = () => import('@/components/KeyIndex.vue')
+const updateProps = () => import('@/components/updateProps.vue')
 // const Animater = () => import(/* webpackChunkName: "12" */'@/components/Animater.vue')
 // const ImageShow = () => import(/* webpackChunkName: "12" */'@/components/ImageShow.vue')
 const route:RouteConfig[] =
   [
     { path: '/', component: Animater },
     { path: '/ImageShow/:id', name: 'ImageShow/:id', component: ImageShow },
-    { path: '/keyIndex', component: KeyIndex }
+    { path: '/keyIndex', component: KeyIndex },
+    { path: '/updateProps', component: updateProps }
   ]
 
 export default new VueRouter({ routes: route })
